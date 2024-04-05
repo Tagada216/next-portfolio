c90aa4fdecf165e9e7d489b2627f75a679923aec
Tagada216 <nicolasdelcourt10@gmail.com>
2024-04-05 10:17:05 +0200

    Add blog section

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -55,16 +55,16 @@ function Navbar() {
               </div>
             </a>
           </li>
-          {/* <li>
+          <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="#blogs"
             >
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                BLOGS
+                Mes Articles
               </div>
             </a>
-          </li> */}
+          </li>
           <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
